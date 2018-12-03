ec854eb5e962955b7790887c0ceb360fe299b4e2
pustel <gundula.pustel@trashmail.de>
2018-12-03 13:58:57 +0100

    Fresh Workspace

---
+++ Hund/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public Main() {
+		// Object von Tierheim erstellen
+		Tierheim dasTierheim =new Tierheim();
+		//programm starten
+		dasTierheim.start();
+	}
+
+}
---
+++ New_light/src/Light.java
@@ -0,0 +1,11 @@
+
+public class Light {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test = "Light";
+			System.out.println("Let There Be ");
+			System.out.print(test);
+	}
+
+}
